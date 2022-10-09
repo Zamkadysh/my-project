02812660f47cf36c467eec9c6965c7ce30675f8d
Zamkadysh <vladimirklimenko001@gmail.com>
2022-10-08 22:20:41 -0500

    main page layout

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-function fun() {
-    ;
-}
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/index.html"))
+})
+
+app.get('/fullcalendar/main.css', (req, res) => {
+    res.sendFile(path.join(__dirname, 'node_modules/fullcalendar/main.css'))
+})
+
+app.get('/fullcalendar/main.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'node_modules/fullcalendar/main.js'))
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
